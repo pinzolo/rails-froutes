9c1919ddb5ca8dac5891cb23f228a5edcb5dd00c
pinzolo <pinzolo@gmail.com>
2014-08-14 19:34:53 +0900

    Add basic feature

--- lib/rails/froutes.rb
+++ lib/rails/froutes.rb
@@ -1,7 +1,10 @@
+require "rails/froutes/routes_inspector_patch"
 require "rails/froutes/version"
 
-module Rails
-  module Froutes
-    # Your code goes here...
-  end
+if Rails::VERSION::MAJOR == 4
+  require 'action_dispatch/routing/inspector'
+  ActionDispatch::Routing::RoutesInspector.__send__(:include, Rails::Froutes::RoutesInspectorPatch)
+else
+  require 'rails/application/route_inspector'
+  Rails::Application::RouteInspector.__send__(:include, Rails::Froutes::RoutesInspectorPatch)
 end
---
+++ lib/rails/froutes/routes_inspector_patch.rb
@@ -0,0 +1,37 @@
+module Rails
+  module Froutes
+    module RoutesInspectorPatch
+      extend ActiveSupport::Concern
+
+      included do
+        alias_method_chain :collect_routes, :fill_name
+      end
+
+      def collect_routes_with_fill_name(base_routes)
+        routes = allowed_to_fill_route_name? ? name_filled_routes_from(base_routes) : base_routes
+        collect_routes_without_fill_name(routes)
+      end
+
+      private
+
+      def allowed_to_fill_route_name?
+        fill_env = ENV['FILL_NAME'] || ENV['fill_name']
+        fill_env && %w(y yes true).include?(fill_env.downcase)
+      end
+
+      def name_filled_routes_from(routes)
+        routes.dup.tap do |dup_routes|
+          controller, name = nil
+          dup_routes.each do |route|
+            if route.name || route.defaults[:controller] != controller
+              name = route.name
+              controller = route.defaults[:controller]
+            else
+              route.instance_variable_set(:@name, name)
+            end
+          end
+        end
+      end
+    end
+  end
+end
